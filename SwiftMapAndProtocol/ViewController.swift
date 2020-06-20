7f94470e87990f6c5ba71f77f6adf118ac763044
Atsushi Adachi <atsuk0r05296@gmail.com>
2020-06-20 13:03:23 +0900

    Initial Commit

---
+++ SwiftMapAndProtocol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftMapAndProtocol
+//
+//  Created by 安達篤史 on 2020/06/20.
+//  Copyright © 2020 Adachi Atsushi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
